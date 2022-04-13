53a7a4b0c6f87dfd2a9bfa180c2dea50cc4b742c
CharlesLee-23 <Coleman.hallom@Spray.com>
2022-04-13 13:55:39 -0500

    stuck on how to get the func to the admin

--- public/javascripts/dbInsert.js
+++
@@ -1,9 +0,0 @@
-import {client, coll} from './conn.js';
-
-export function setChallWord(word, clue){
-
-if(clue!=null || word!=null || word !=""|| clue !=""){
-	client.connect(
-		coll.insertOne({'date':'01/01/2023', 'wordOfDay':word, 'wordClue':clue}));
-	}
-}
